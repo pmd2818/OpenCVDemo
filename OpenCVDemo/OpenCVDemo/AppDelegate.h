f8f7c87046a2eb903d4b3a4fcea8f6619bea3f72
boljonggo <pmd2818@163.com>
2018-10-16 19:05:39 +0800

    OpenCVDemo init.

---
+++ OpenCVDemo/OpenCVDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OpenCVDemo
+//
+//  Created by boljonggo on 2018/10/16.
+//  Copyright © 2018 boljonggo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OpenCVDemo/OpenCVDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OpenCVDemo
+//
+//  Created by boljonggo on 2018/10/16.
+//  Copyright © 2018 boljonggo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
